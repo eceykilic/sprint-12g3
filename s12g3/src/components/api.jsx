fec0cb8865518b598f94a5c2a8f339a4173ff0bb
eceykilic <eceykilic@gmail.com>
2024-02-07 12:20:25 +0300

    token part added

--- s12g3/src/components/api.jsx
+++ s12g3/src/components/api.jsx
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
-const token = 'your_actual_jwt_token'; 
 
 // Create an Axios instance with a base URL and default headers
 const api = axios.create({
     baseURL: 'https://fakestoreapi.com',
     headers: {
       'Content-Type': 'products',
-      'Authorization': `Bearer ${token}`,
+      'Authorization': "token",
     },
   });
 
